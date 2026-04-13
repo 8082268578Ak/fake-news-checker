43bf8d006c586233347903d7091bdb6d0bfaaff3
Amit kumar <120185627+8082268578Ak@users.noreply.github.com>
2026-04-13 13:47:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 # Load model & vectorizer
 model = pickle.load(open("model.pkl", "rb"))
-vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
+vectorizer = pickle.load(open("vectorizer.pkl", "rb"))#hhhhh
 
 @app.route('/')
 def home():
