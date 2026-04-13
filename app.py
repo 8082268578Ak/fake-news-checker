774f9bf28a32fdbee1dc8782f819ac4e910165be
Amit kumar <120185627+8082268578Ak@users.noreply.github.com>
2026-04-13 13:45:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ vectorizer = pickle.load(open("vectorizer.pkl", "rb"))
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template("index.html")#hhhhh
 #ghh
 @app.route('/predict', methods=['POST'])
 def predict():
